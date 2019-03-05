e7bd7c5fdbabb5eaaf803b9524bc75ce1f14cd48
叶作垚 <yezuoyao@xiezuoyaodeMacBook-Pro.local>
2019-03-05 13:24:14 +0800

    all

---
+++ src/main/java/com/example/apigateway/filter/AddHeaderFilter.java
@@ -0,0 +1,57 @@
+package com.example.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.UUID;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Description:  ---——require需求|ask问题|jira
+ * Design :  ----the  design about train of thought 设计思路
+ * User: yezuoyao
+ * Date: 2019-02-17
+ * Time: 19:29
+ * Email:yezuoyao@huli.com
+ *
+ * @author yezuoyao
+ * @since 1.0-SNAPSHOT
+ */
+@Component
+public class AddHeaderFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        if(request.getRequestURI().equals("/login")){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletResponse response = requestContext.getResponse();
+        response.setHeader("myadd", UUID.randomUUID().toString());
+        return null;
+    }
+}
